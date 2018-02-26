41f5a94cce91844c732de6ded51c2ce93700e688
Unknown <dlu.duncanlu@gmail.com>
2018-02-25 20:44:56 -0800

    Implemented UI + Enemy Attacking Script  + PlayerHealth Script

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+    Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
+    
+    bool playerInRange;
+
+    float timer;
+
+	// Use this for initialization
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        anim = GetComponent<Animator>();
+
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit (Collider other)
+    {
+        if(other.gameObject == player)
+        {
+
+            playerInRange = false;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        timer += Time.deltaTime;
+
+        if(timer >= timeBetweenAttacks && playerInRange)
+        {
+            Attack();
+        }
+
+        if playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("PlayerDead");
+        }
+	}
+
+    void Attack()
+    {
+        timer = 0f;
+
+        if(playerHealth.currentHealth >0)
+        {
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public AudioClip deathClip;
+    public float flashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+    Animator anim;
+    AudioSource playerAudio;
+    PlayerAnimation playerAnim;
+
+    bool isDead;
+    bool damaged;
+
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerAnim = GetComponent<PlayerAnimation>();
+
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (damaged)
+        {
+            damageImage.color = flashColor;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        damaged = false;
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+
+        healthSlider.value = currentHealth;
+
+        playerAudio.Play();
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        anim.SetTrigger("Die");
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+        playerAnim.enabled = false;
+    }
+}
