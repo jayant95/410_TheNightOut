8668a180c11288f3397e61b3564c863919130d84
Jayant Tailor <jayantt@sfu.ca>
2018-02-27 16:11:09 -0800

    Add drunk enemy who throws random projectiles
    Enemy throws random pieces of garbage in any direction when the player is close enough. Colliders are in so we can decrease player/cop health in the future when health is incorporated

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public GameObject currentPlayer;
+	private Transform playerTransform;
+	private float garbageInterval = 2.0f;
+	private float throwRate = 2.0f;
+	public Transform throwPoint;
+	public GameObject garbagePiece;
+	private float minDistance = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		playerTransform = currentPlayer.GetComponent<PlayerSwitch>().currentTransform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerTransform = currentPlayer.GetComponent<PlayerSwitch>().currentTransform;
+
+
+		if (Vector2.Distance (transform.position, playerTransform.position) <= minDistance) {
+			if (Time.time > garbageInterval) {
+				garbageInterval = Time.time + throwRate;
+				Instantiate (garbagePiece, throwPoint.position, throwPoint.rotation);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour {
+
+	private int lifeTimer = 0;
+	public GameObject currentPlayer;
+	private GameObject playerObject;
+
+	// Use this for initialization
+	void Start () {
+		playerObject = currentPlayer.GetComponent<PlayerSwitch> ().currentPlayer;
+		this.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (Random.Range(-5.0f, 5.0f), Random.Range(2.0f, 2.0f));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifeTimer++;
+
+		if (lifeTimer > 90) {
+			this.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+			//this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 
+			Physics.gravity = new Vector2(0, 0);
+			if (gameObject.name == "garbage(Clone)") {
+				Destroy (gameObject, 3);
+			}
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			Destroy (gameObject, 1);
+			Debug.Log ("Hit Player!");
+			// Decrease player health
+		}
+
+		if (other.name == "Cop") {
+			Destroy (gameObject, 1);
+			Debug.Log ("Hit Cop!");
+			// Decrease copy health
+		}
+
+	}
+}
