3341070346a61cc416d5aeeeccd65c7007167aca
Jayant Tailor <jayantt@sfu.ca>
2018-03-03 15:48:17 -0800

    Added friendly AI
    Your companion now follows you where you go. Used the attract script and tweaked it

---
+++ Assets/Scripts/FriendlyAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendlyAI : MonoBehaviour {
+	
+	public GameObject currentPlayer;
+	private Transform playerTransform;
+	private GameObject playerObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
